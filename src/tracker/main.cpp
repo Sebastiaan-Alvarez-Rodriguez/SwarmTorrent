06220110d2c7013beabddd2e6134c39fa2ba4a04
Mariska <mariska-ijpelaar@hotmail.com>
2020-11-23 22:34:52 +0100

    busy setting up connections

--- src/tracker/main.cpp
+++ src/tracker/main.cpp
@@ -2,30 +2,46 @@
 #include <tclap/CmdLine.h>
 #include <stdexcept>
 
-#include "peerTable/peerTable.h"
+#include "shared/connection/impl/TCP/in/TCPInConnection.h"
+#include "shared/torrent/ipTable/ipTable.h"
 #include "shared/util/print.h"
 
 
-bool setup(int argc, char const ** argv, IPTable& pt) {
+bool setup(int argc, char const ** argv, IPTable& pt, std::unique_ptr<Connection>& tracker_conn) {
     TCLAP::CmdLine cmd("SwarmTorrent Tracker Main", ' ', "0.1");
     TCLAP::ValueArg<std::string> seederArg("i", "initial-seeder", "IP of initial seeder, format: [TCP]:[4/6]:PORT:IP", true, "", "IP Strings", cmd);
+    TCLAP::ValueArg<unsigned> portArg("p", "port", "Specify on which port to listen", true, 0, "unsigned", cmd);
 
     cmd.parse(argc, argv);
 
     std::string initial_seeder = seederArg.getValue();
     try {
-        pt = peertable::init(initial_seeder);
+        std::vector<std::string> peers = {initial_seeder};
+        pt = IPTable::from(peers);
     } catch (const std::exception& e) {
-        std::cerr << print::RED << "[ERROR] " << e.what() << std::endl;
+        std::cerr << print::RED << "[ERROR] Could not setup PeerTable: " << e.what() << std::endl;
         return false;
     }
+
+    unsigned tracker_port = portArg.getValue();
+    if (tracker_port < 1000) 
+        std::cerr << print::YELLOW << "[WARNING] Port numbers lower than 1000 may be reserved by the OS!"<< print::CLEAR << std::endl;
+
+    tracker_conn = TCPInConnection::Factory::from(NetType::IPv4).withPort(tracker_port).create();
+    if (tracker_conn->get_state() != Connection::READY) {
+        std::cerr << print::RED << "[ERROR] Could not initialize connection" << std::endl;
+        return false;
+    }
+
     return true;
 }
 
+//TODO: change to multiple torrentfiles
 int main(int argc, char const **argv) {
     IPTable pt;
-    if (!setup(argc, argv, pt))
+    std::unique_ptr<Connection> tracker_conn;
+    if (!setup(argc, argv, pt, tracker_conn))
         return 1;
-    //TODO: run() or listen() or anything else
+    
     return 0;
 }
--- src/tracker/peerTable/peerTable.cpp
+++
@@ -1,6 +0,0 @@
-#include "peerTable.h"
-
-IPTable peertable::init(std::string initial_seeder) {
-    std::vector<std::string> peers = {initial_seeder};
-    return IPTable::from(peers);
-}
