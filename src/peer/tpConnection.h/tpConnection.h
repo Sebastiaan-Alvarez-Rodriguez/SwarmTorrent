bedc9864d8af6ca4c3ba46450b94c937512d065d
Mariska <mariska-ijpelaar@hotmail.com>
2020-11-25 19:58:33 +0100

    small change

--- src/peer/tpConnection.h/tpConnection.h
+++ src/peer/tpConnection.h/tpConnection.h
@@ -11,6 +11,5 @@ bool subscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash
 bool unsubscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash, Addr peer);
 bool retrieve(std::unique_ptr<Connection>& connection, std::string torrent_hash, IPTable& peertable);
 //bool update(std::unique_ptr<Connection> connection, std::string torrent_hash, ); 
-//STATUS request_and_receive(std::unique_ptr<Connection> connection, TAG tag, void* const request, unsigned request_length, const void* const message, unsigned message_length);
 
 #endif
