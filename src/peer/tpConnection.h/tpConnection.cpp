eb45f9f9f2030fa03c5308863ccf87c89f889ea4
Mariska <mariska-ijpelaar@hotmail.com>
2020-11-25 19:15:47 +0100

    still busy

--- src/peer/tpConnection.h/tpConnection.cpp
+++ src/peer/tpConnection.h/tpConnection.cpp
@@ -4,7 +4,7 @@
 #include "tpConnection.h"
 #include "shared/connection/tracker/trackerConnection.h"
 
-static void request(std::unique_ptr<Connection> connection, Header header, size_t msg_length, void* const msg) {
+static void request(std::unique_ptr<Connection>& connection, Header header, size_t msg_length, const void* const msg) {
     void* const buf = malloc(sizeof(Header) + msg_length);
     memcpy(buf, &header, sizeof(Header));
     memcpy((uint8_t*)buf + sizeof(Header), msg, msg_length);
@@ -13,7 +13,7 @@ static void request(std::unique_ptr<Connection> connection, Header header, size_
     free(buf);
 }
 
-static STATUS receive(std::unique_ptr<Connection> connection, size_t& msg_length, void* const msg) {
+static STATUS receive(std::unique_ptr<Connection>& connection, size_t& msg_length, void* msg) {
     Header header;
     connection->recvmsg(&header, sizeof(Header));
 
@@ -27,25 +27,28 @@ static STATUS receive(std::unique_ptr<Connection> connection, size_t& msg_length
     return header.status;
 }
 
-bool subscribe(std::unique_ptr<Connection> connection, std::string torrent_hash, Addr peer) {    std::stringstream hstream; 
+bool subscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash, Addr peer) {    std::stringstream hstream; 
     peer.write_stream(hstream);
-    size_t msg_length = hstream.str().size(); 
+    std::string tmp = hstream.str();
+    size_t msg_length = tmp.size(); 
     Header header = {SUBSCRIBE, REQUEST, msg_length};
-    char* msg = hstream.c_str();
+    const char* msg = tmp.c_str();
 
     request(connection, header, msg_length, msg);
 
-    if (receive(connection, msg_length, msg) == ERROR)
+    char* received;
+    if (receive(connection, msg_length, received) == ERROR)
         return false;
 
-    free(msg);
+    free(received);
     return true;
 }
 
-bool unsubscribe(std::unique_ptr<Connection> connection, std::string torrent_hash, Addr peer) {
+bool unsubscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash, Addr peer) {
+    return true;
 
 }
 
-bool retrieve(std::unique_ptr<Connection> connection, std::string torrent_hash, IPTable& peertable) {
-
+bool retrieve(std::unique_ptr<Connection>& connection, std::string torrent_hash, IPTable& peertable) {
+    return true;
 }
--- src/peer/tpConnection.h/tpConnection.h
+++ src/peer/tpConnection.h/tpConnection.h
@@ -7,9 +7,9 @@
 #include "shared/connection/impl/TCP/TCPConnection.h"
 #include "shared/torrent/ipTable/ipTable.h"
 
-bool subscribe(std::unique_ptr<Connection> connection, std::string torrent_hash, Addr peer);
-bool unsubscribe(std::unique_ptr<Connection> connection, std::string torrent_hash, Addr peer);
-bool retrieve(std::unique_ptr<Connection> connection, std::string torrent_hash, IPTable& peertable);
+bool subscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash, Addr peer);
+bool unsubscribe(std::unique_ptr<Connection>& connection, std::string torrent_hash, Addr peer);
+bool retrieve(std::unique_ptr<Connection>& connection, std::string torrent_hash, IPTable& peertable);
 //bool update(std::unique_ptr<Connection> connection, std::string torrent_hash, ); 
 //STATUS request_and_receive(std::unique_ptr<Connection> connection, TAG tag, void* const request, unsigned request_length, const void* const message, unsigned message_length);
 
